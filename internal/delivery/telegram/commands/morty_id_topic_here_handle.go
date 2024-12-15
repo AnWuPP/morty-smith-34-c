ae9756117164b5f394a776d088c0f72ca98f805a
AnWu <anwu.cpp@gmail.com>
2024-12-15 11:50:20 +0300

    Add debug / info / error work with logger SLUG

--- internal/delivery/telegram/commands/morty_id_topic_here_handle.go
+++ internal/delivery/telegram/commands/morty_id_topic_here_handle.go
@@ -2,7 +2,6 @@ package commands
 
 import (
 	"context"
-	"log"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
@@ -11,7 +10,7 @@ import (
 func (h *CommandHandler) handleMortyIdTopicHere(ctx context.Context, b *bot.Bot, msg *models.Message) {
 	err := h.ChatUseCase.UpdateThreadID(ctx, msg.Chat.ID, msg.MessageThreadID)
 	if err != nil {
-		log.Printf("Failed to set ID topic: %v", err)
+		h.logger.Error(ctx, "handleMortyIdTopicHere: errror save topic. user: %v | chat: %v | err: %v", msg.From, msg.Chat, err)
 		b.SendMessage(ctx, &bot.SendMessageParams{
 			ChatID:          msg.Chat.ID,
 			MessageThreadID: msg.MessageThreadID,
@@ -19,6 +18,7 @@ func (h *CommandHandler) handleMortyIdTopicHere(ctx context.Context, b *bot.Bot,
 		})
 		return
 	}
+	h.logger.Debug(ctx, "handleMortyIdTopicHere: errror save topic. user: %v | chat: %v", msg.From, msg.Chat)
 	h.chatCache.SetThreadID(msg.Chat.ID, msg.MessageThreadID)
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID:          msg.Chat.ID,
--- internal/delivery/telegram/commands/unmute_handler.go
+++ internal/delivery/telegram/commands/unmute_handler.go
@@ -12,7 +12,8 @@ import (
 
 // handleUnmute handles the /unmute command
 func (h *CommandHandler) handleUnmute(ctx context.Context, b *bot.Bot, msg *models.Message) {
-	if msg.ReplyToMessage == nil {
+	if msg.ReplyToMessage == nil || msg.ReplyToMessage.ID == msg.ReplyToMessage.MessageThreadID {
+		h.logger.Debug(ctx, "handleUnmute: message is not reply. text: %s | user: %v | chat: %v", msg.Text, msg.From, msg.Chat)
 		return
 	}
 
@@ -35,6 +36,7 @@ func (h *CommandHandler) handleUnmute(ctx context.Context, b *bot.Bot, msg *mode
 		UntilDate: int(time.Now().Add(time.Second * 30).Unix()),
 	})
 	if err != nil {
+		h.logger.Debug(ctx, "handleUnmute: message is not reply. text: %s | user: %v | for: %v | chat: %v | err: %v", msg.Text, msg.From, msg.ReplyToMessage.From, msg.Chat, err)
 		b.SendMessage(ctx, &bot.SendMessageParams{
 			ChatID: msg.Chat.ID,
 			Text:   fmt.Sprintf("О нет\\! Не могу размутить %s\\, %s\\, помоги мне\\!", telegram.GenerateMention(msg.ReplyToMessage.From), telegram.GenerateMention(msg.From)),
@@ -46,6 +48,7 @@ func (h *CommandHandler) handleUnmute(ctx context.Context, b *bot.Bot, msg *mode
 		return
 	}
 
+	h.logger.Info(ctx, "handleUnmute: unmute. text: %s | user: %v | for: %v | chat: %v", msg.Text, msg.From, msg.ReplyToMessage.From, msg.Chat)
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: msg.Chat.ID,
 		Text:   fmt.Sprintf("Фух\\! %s размучен\\, %s\\, ты герой\\!", telegram.GenerateMention(msg.ReplyToMessage.From), telegram.GenerateMention(msg.From)),
